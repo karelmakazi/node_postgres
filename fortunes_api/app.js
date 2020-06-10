aa91150b213e79a516ee5c4fad59db6fc7e02d1e
Karel Fourie <karelmakazi@gmail.com>
2020-06-10 16:42:34 +1200

    Finish Fortunes API

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -7,6 +7,8 @@ const app = express()
 
 app.use(bodyParser.json())
 
+
+//GET 
 app.get('/fortunes', (req, res) => {
   res.json(fortunes)
 })
@@ -19,10 +21,14 @@ app.get('/fortunes/:id', (req, res) => {
   res.json(fortunes.find(f => f.id == req.params.id))
 })
 
+//POST & PUT
+const writeFortunes = json => {
+  fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err))
+}
+
 app.post('/fortunes', (req, res) => {
   const { message, lucky_number, spirit_animal } = req.body
   const fortune_ids = fortunes.map(f => f.id)
-
  
   const new_fortunes = fortunes.concat({ 
     id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
@@ -31,11 +37,32 @@ app.post('/fortunes', (req, res) => {
     spirit_animal
   })
 
-  fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err))
-  
-
+  writeFortunes(new_fortunes)
   res.json(new_fortunes)
 })
 
+app.put('/fortunes/:id', (req, res) => {
+  const { id } = req.params
+
+  //Finds fortune by id, and replaces values if keys present matches strings given
+  const old_fortune = fortunes.find(f => f.id == id)
+   ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+    if(req.body[key]) old_fortune[key] = req.body[key]
+  })
+
+  writeFortunes(fortunes)
+  res.json(fortunes)
+})
+
+//DELETE
+app.delete('/fortunes/:id', (req, res) => {
+  const { id } = req.params
+  
+  //Creates array excluding given id
+  const new_fortunes = fortunes.filter(f => f.id != id)
+
+  writeFortunes(new_fortunes)
+  res.json(new_fortunes)
+})
 
 module.exports = app
